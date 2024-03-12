6f73930fd1613610d641605bb6e653b8a0dd90a2
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2024-03-11 22:51:36 -0600

    Adding lazy.vim plugin manager

--- init.lua
+++ init.lua
@@ -8,5 +8,41 @@ vim.opt.wrap = false
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
+-- Sets up lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+    }
+})
+
+vim.cmd.colorscheme("tokyonight-storm")
