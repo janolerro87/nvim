934fdf6cfbd742915eb62b7eee61dc29e102930d
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2024-07-28 22:23:32 -0600

    New plugins:   - Which-Key   - TODO comments

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -52,5 +52,55 @@ return {
 
       vim.keymap.set("n", "st", whitespace.trim)
     end
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = "VeryLazy",
+    opts = {},
+    -- config = function ()
+    --     local tc = require("todo-comments")
+    --     vim.keymap.set("n", "]t", function()
+    --         tc.jump_next()
+    --     end, { desc = "Next todo comment" })
+    --
+    --     vim.keymap.set("n", "[t", function()
+    --         tc.jump_prev()
+    --     end, { desc = "Previous todo comment" })
+    --
+    --     -- You can also specify a list of valid jump keywords
+    --
+    --     vim.keymap.set("n", "]t", function()
+    --         tc.jump_next({keywords = { "ERROR", "WARNING" }})
+    --     end, { desc = "Next error/warning todo comment" })
+    -- end,
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
   }
 }
