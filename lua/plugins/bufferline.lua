0ab9848d14b8215559d8ebc699733b7d11029a01
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2024-07-28 22:22:31 -0600

    Fix:
      - Remove lsp diagnostics on bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,7 +14,6 @@ return {
         show_buffer_close_icons = false,
         color_icons = false,
         diagnostics = "nvim_lsp",
-        diagnostics_update_in_insert = true,
         diagnostics_indicator = function (_, _, diagnostics_dict)
           local s = " "
           for e, n in pairs(diagnostics_dict) do
