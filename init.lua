3e82d37d5993cd19dba0808c806501c9d6c9af75
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2025-04-27 15:46:50 -0600

    Update Lazy init

--- init.lua
+++ init.lua
@@ -1,28 +1,6 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("vim-options")
 require("vim-keymaps")
-require("lazy").setup({
-  spec = "plugins",
-  change_detection = { notify = false },
-  checker = {
-    enabled = true,
-    notify = true,
-    frequency = 3600
-  }
-})
-
+require("config.lazy")
 
 vim.keymap.set("n", "<leader>l", ":Lazy<Return>")
 vim.keymap.set("n", "<leader>m", ":Mason<Return>")
