2ef3373a9e9da4142664ccbba19abc47f6e0f4aa
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2025-05-01 13:00:26 -0600

    Fix Trouble keybinds

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,17 +4,22 @@ return {
     config = function ()
       require("trouble").setup()
 
-      vim.keymap.set("n", "<leader>tt", function ()
-        require("trouble").toggle()
-      end)
+      -- vim.keymap.set("n", "<leader>tt", function ()
+      --   require("trouble").toggle()
+      -- end)
 
       vim.keymap.set("n", "<leader>tn", function ()
         require("trouble").next({ skip_group = true, jump = true})
       end)
 
       vim.keymap.set("n", "<leader>tp", function ()
-        require("trouble").previous({ skip_group = true, jump = true})
+        require("trouble").prev({ skip_group = true, jump = true})
       end)
-    end
+    end,
+    keys = {
+      { "<leader>tt", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>" },
+      { "<leader>tl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    }
   }
 }
