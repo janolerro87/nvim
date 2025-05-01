0b72d18733b80e1b076e911be35dc72bc626b7ab
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2025-05-01 13:04:10 -0600

    Added dap debugging adapter for csharp

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -12,6 +12,23 @@ return {
     require("dapui").setup()
     require("dap-go").setup()
 
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "/usr/bin/netcoredbg",
+      args = { '--interpreter=vscode' }
+    }
+
+    dap.configurations.cs = {
+        {
+            type = "coreclr",
+            name = "launch - netcoredbg",
+            request = "launch",
+            program = function()
+                return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug", "file")
+            end,
+        }
+    }
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
