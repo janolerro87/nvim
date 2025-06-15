943421fee730be305d61460667271b4196127fc8
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2025-06-14 19:20:40 -0600

    Disable Alpha plugin and enable Snacks plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,43 +1,43 @@
 return {
-
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
-
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
-
-    _Gopts = {
-      position = "center",
-      hl = "Type",
-      -- wrap = "overflow";
-    }
-
-    -- local function footer()
-    -- 	return "Haskell can suck mo' nads"
-    -- end
-
-    -- dashboard.section.footer.val = footer()
-
-    -- dashboard.opts.opts.noautocmd = true
-    alpha.setup(dashboard.opts)
-  end,
+--
+--   "goolord/alpha-nvim",
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--
+--   config = function()
+--     local alpha = require("alpha")
+--     local dashboard = require("alpha.themes.startify")
+--
+--     dashboard.section.header.val = {
+--       [[                                                                       ]],
+--       [[                                                                       ]],
+--       [[                                                                     ]],
+--       [[       ████ ██████           █████      ██                     ]],
+--       [[      ███████████             █████                             ]],
+--       [[      █████████ ███████████████████ ███   ███████████   ]],
+--       [[     █████████  ███    █████████████ █████ ██████████████   ]],
+--       [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+--       [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+--       [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+--       [[                                                                       ]],
+--       [[                                                                       ]],
+--       [[                                                                       ]],
+--     }
+--
+--     _Gopts = {
+--       position = "center",
+--       hl = "Type",
+--       -- wrap = "overflow";
+--     }
+--
+--     -- local function footer()
+--     -- 	return "Haskell can suck mo' nads"
+--     -- end
+--
+--     -- dashboard.section.footer.val = footer()
+--
+--     -- dashboard.opts.opts.noautocmd = true
+--     alpha.setup(dashboard.opts)
+--   end,
 }
