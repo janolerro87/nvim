6772491be11dfdfa2df8054d00f042c90a7a869e
Javier Norberto Lerma Robles <janolerro87@gmail.com>
2025-07-30 21:49:58 -0600

    Activate LSP diagnostics on virtual lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = " "
-
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
@@ -62,14 +60,52 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
 
-vim.cmd([[
-  augroup highlight_yank
-  autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
-  augroup END
-]])
-
 vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" })
 
 vim.g.python3_host_prog = "/usr/bin/python"
 vim.g.loaded_perl_provider = 0
+
+vim.hl.priorities.semantic_tokens = 95
+
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = "●",
+    format = function(diagnostic)
+      local code = diagnostic.code and string.format("[%s]", diagnostic.code) or ""
+      return string.format("%s %s", code, diagnostic.message)
+    end,
+    update_in_insert = true,
+    float = {
+      source = true,
+    },
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = ' ',
+        [vim.diagnostic.severity.WARN] = ' ',
+        [vim.diagnostic.severity.INFO] = ' ',
+        [vim.diagnostic.severity.HINT] = '󰌵 ',
+      },
+    },
+    on_ready = function ()
+      vim.cmd "highlight DiagnosticVirtualText guibg=NONE"
+    end,
+  }
+}
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.hl.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
+
+vim.cmd [[
+  augroup kitty_mp
+  autocmd!
+  au VimLeave * :silent !kitty @ set-spacing padding=default margin=default
+  au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0 3 0 3
+  augroup END
+]]
